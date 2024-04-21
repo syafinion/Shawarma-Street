69c5596fd4ba39c786dbd3401dbffe9c5d9d10f7
syafinion <syafiqmajid286@gmail.com>
2024-04-22 05:05:18 +0800

    push new update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Home page
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+// Cart page
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+// Checkout page
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+// Menu page
+Route::get('/restaurantmenu', function () {
+    return view('restaurantmenu');
 });
